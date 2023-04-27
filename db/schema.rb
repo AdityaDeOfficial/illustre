6b29e36608e4fca2e7244f47219bbb02bad41547
Mary Chan <marychancfy@gmail.com>
2023-04-28 03:30:25 +1000

    add details to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_29_015752) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_29_015753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_29_015752) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "nickname"
+    t.text "bio"
+    t.string "instagram"
+    t.string "twitter"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
