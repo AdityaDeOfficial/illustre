21d0b050801501becd3f585dbeeccdd42a235feb
Aditya De <adityadeofficial@gmail.com>
2023-04-29 22:18:23 +1000

    manga upload page skeleton done

---
+++ app/controllers/mangas_controller.rb
@@ -0,0 +1,21 @@
+class MangasController < ApplicationController
+  def new
+    @manga = Manga.new
+  end
+
+  def create
+    @manga = Manga.new(manga_params)
+    @manga.user_id = current_user.id
+    if @manga.save
+      redirect_to @manga, notice: 'Manga was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def manga_params
+    params.require(:manga).permit(:title, :description, :user_id, :cover_image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :mangas
+  get 'upload_manga', to: 'mangas#new'
 end
