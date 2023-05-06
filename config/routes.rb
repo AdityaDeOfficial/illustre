965e6df882fb4578d3306d65ad58ec1a58c7522a
Aditya De <adityadeofficial@gmail.com>
2023-05-06 12:54:54 +1000

    added seed file and updated controllers for manga and chapters and view files added scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root 'mangas#index'
+  resources :mangas do
+    resources :chapters, shallow: true
+  end
 
- # chapters 
- resources :mangas
-  get 'upload_manga', to: 'mangas#new'
-   resources :chapters do
-     # for access /chapters/view
-     get '/mangas/:manga_id/chapters/:id', to: 'chapters#show'
-     # for access /chapters/new
-     get '/mangas/:manga_id/chapters/new', to: 'chapters#new'
-   end
- 
  resources :users
  get "dashboard", to: "pages#dashboard"
 end
